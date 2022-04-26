74aa17f2ac248df3ab25b181196de0d0911e48dd
Cade Keenan <95306859+cadekeenan@users.noreply.github.com>
2022-04-25 21:25:39 -0500

    appUpdate

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
+from sqlite3 import complete_statement
 from flask import Flask
 from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return render_template('index.html', pageTitle= "Home")
+def index():
+    return render_template('index.html', pageTitle='VTM Site')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', pageTitle="About")
-@app.route('/estimates', methods=['GET', 'POST'])
+    return render_template('about.html', pageTitle='About Page')
+
+def calculate_top(radius):
+    return 3.14*(radius**2)
+
+def calculate_side(radius,height):
+    return 2*(3.14*(radius*height))
+
+
+@app.route('/estimate', methods=['GET','POST'])
 def estimate():
     if request.method == 'POST':
         form = request.form
-        radius = form(['radius'])
-        height = form(['height'])
-        radius = float(radius)
-        height = float(height)
-        area_top = 3.14 * radius**2
-        area_side = 2*3.14*radius*height
-        total_area = area_top + area_side
-        sqft = total_area / 144
-        matCost = 25 
-        totalMatCost = sqft * matCost
-        labor = 15
-        total_labor_cost = labor * sqft
-        total_cost_estimate = totalMatCost + total_labor_cost
-        total_cost_estimate = str(total_cost_estimate)
-        return(total_cost_estimate)
-    return(render_template('estimates.html', pageTitle="Estimate")) 
+        radius = float(form['radius'])
+        height = float(form['height'])
+        top_area=3.14*(radius**2)
+        side_area=2*(3.14*(radius*height))
+        total_area = top_area + side_area
+        total_sqft = total_area/144
+        material_cost = 25 
+        total_material_cost = total_sqft*material_cost
+        labor_cost = 15
+        total_labor_cost = labor_cost*total_sqft
+        cost = total_material_cost + total_labor_cost
+        return str("${:,.2f}".format(cost))
+    return render_template('estimate.html', pageTitle='Estimate')
 
 if __name__ == '__main__':
     app.run(debug=True)
