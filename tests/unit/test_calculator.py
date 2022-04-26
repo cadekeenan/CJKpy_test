d7974abeae685e3bf886745c70d0b55157528dbd
Cade Keenan <95306859+cadekeenan@users.noreply.github.com>
2022-04-25 21:27:31 -0500

    Bert

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -4,26 +4,24 @@
 # https://docs.python.org/3/library/unittest.html#unittest.TestCase.debug
 
 # python3 -m unittest test_calculator.py
-
+import app
 import unittest
 import tests.calculator as calc
 
-class TestCalculator(unittest.TestCase):  #test class
-    
-import app
+class TestCalculator(unittest.TestCase):  
 
-def  test_calculate_top_area(): 
-    """
+    def  test_calculate_top(): 
+        """
     GIVEN a user enters 180 as the radius 
     WHEN that radius is passed to this function 
     THEN the user's top area is accurately created
     """
-    assert app.calculate_top_area(180)== 101736
+    assert app.calculate_top(180)== 101736
 
-def  test_calculate_side_area(): 
+def  test_calculate_side(): 
     """
     GIVEN a user enters 180 as the radius and 360 as height
     WHEN that radius and height is passed to this function 
     THEN the user's side area is accurately created
     """
-    assert app.calculate_side_area(180,360)== 406944
+    assert app.calculate_side(180,360)== 406944
